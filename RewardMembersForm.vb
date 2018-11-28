6926de62f0026ee3bbeff54b1551394f485bc89d
Hiya Guha <hiyaguha11@gmail.com>
2018-11-28 15:54:10 -0500

    minor

--- RewardMembersForm.vb
+++ RewardMembersForm.vb
@@ -27,14 +27,20 @@ Public Class RewardMembersForm
 
     Private Sub AddToCart_Rewards_Click(sender As Object, e As EventArgs) Handles AddToCart_Rewards.Click
         If gold.Checked = True And platinum.Checked = True Then
-            MessageBox.Show("Do not check both boxs")
+            MessageBox.Show("Do not check both boxes")
         ElseIf gold.Checked Then
             custRe = 1
             MessageBox.Show("Added")
         ElseIf platinum.Checked Then
             custRe = 2
             MessageBox.Show("Added")
+        ElseIf Employee.Checked Then
+            If (GlobalVariables.adminID Is Nothing And GlobalVariables.staffID Is Nothing) Then
+                    MessageBox.Show("You cannot add an employee reward because 
+                    you are not an employee!")
+                End If
+            End If
+        Else CustRe = 3
         End If
-
     End Sub
 End Class
